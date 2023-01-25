1c2852c6bd68b22446e97939f3622b6e4792e600
Simon Arlott <sa.me.uk>
2023-01-25 21:36:59 +0000

    platformio 6.1.6 breaks builds

--- vars/platformioApplicationPipeline.groovy
+++ vars/platformioApplicationPipeline.groovy
@@ -45,7 +45,7 @@ def call() {
 			}
 			stage("Prepare") {
 				steps {
-					sh "pipenv install platformio"
+					sh "pipenv install platformio==6.1.5"
 					sh "pipenv graph"
 					lock("NODE=${NODE_NAME} APP=platformio") {
 						sh "pipenv run platformio update"
